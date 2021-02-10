0f3e6a4b1e62fe5bb455b5589950ff48b1c0de11
guilhermeabel99 <gpeuvis@gmail.com>
2021-02-10 20:53:54 -0300

    new

--- index.js
+++ index.js
@@ -42,6 +42,15 @@ bot.on('message', async message => {
       .setFooter(`Ping`, bot.user.displayAvatarURL);
     let msg = await message.channel.send(embed1);
 
+    let embed2 = new RichEmbed()
+    .setTimestamp()
+    .setTitle(`Pong!`)
+    .setColor('#ee3434')
+    .setDescription(`A Latência é ${Math.floor(msg.createdTimestamp - message.createdTimestamp)}ms.\nA Latência da API é ${Math.round(bot.ping)}ms.`)
+    .setFooter(`Ping`, bot.user.displayAvatarURL);
+  msg.edit(embed2);
+}
+
 
 
 
