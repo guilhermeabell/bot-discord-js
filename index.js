3e4b52a5319efba6e65063b248d1fc2e44e99f2d
guilhermeabel99 <gpeuvis@gmail.com>
2021-02-10 20:54:26 -0300

    command say

--- index.js
+++ index.js
@@ -51,6 +51,28 @@ bot.on('message', async message => {
   msg.edit(embed2);
 }
 
+//Comando Say
+if (cmd == "say") {
+  if (message.deletable) message.delete();
+
+  if (args.length < 0) return message.reply(`Não tem nada para falar?`).then(m => m.delete(5000));
+
+  let roleColor = message.guild.me.displayHexColor;
+
+  if (args[0].toLowerCase() === "embed") {
+    let embed = new RichEmbed()
+      .setTimestamp()
+      .setTitle(`${message.author.username}`)
+      .setDescription(args.slice(1).join(" "))
+      .setColor(roleColor === "#000000" ? "#ffffff" : roleColor)
+
+    message.channel.send(embed);
+  } else {
+    message.channel.send(args.join(" "));
+  }
+}
+})
+
 
 
 
