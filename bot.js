1668210cc5ecad08efb6aab228ff54870f2a27a4
guilhermeabel99 <gpeuvis@gmail.com>
2021-02-10 21:17:28 -0300

    bot ajax

--- bot.js
+++ bot.js
@@ -9,16 +9,16 @@ config({
 })
 
 bot.on('ready', () => {
-  console.log(`O ${bot.user.username} foi iniciado com sucesso! Com ${bot.users.size} usuários, ${bot.channels.size} canais e ${bot.guilds.size} servidores.`);
+  console.log(`O ${bot.user.username} foi iniciado com sucesso! Com ${bot.users.cache.size} usuários, ${bot.channels.size} canais e ${bot.guilds.size} servidores.`);
   let status = [
     { name: `Hello World!`, type: 'PLAYING' },
     { name: `Subscribe for channel!`, type: 'WATCHING' },
-    { name: `${bot.users.size} pessoas!`, type: 'LISTENING' },
+    { name: `${bot.users.cache.size} pessoas!`, type: 'LISTENING' },
     { name: `Bot development.`, type: 'STREAMING', url: 'https://twitch.tv/SuaTwitch' }
   ]
   function setStatus() { // Function para mudar status do bot aleatoriamente;
     let randomStatus = status[Math.floor(Math.random() * status.length)]
-    bot.user.setPresence({ game: randomStatus })
+    bot.user.setPresence({ activity: randomStatus }) // game: randomStatus offline;
   }
   setStatus();
   setInterval(() => setStatus(), 5000)
